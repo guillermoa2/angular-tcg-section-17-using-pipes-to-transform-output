fea9a7847b790511f29650d0329dca209a1df4e9
Guillermo Acosta <guillermo222@att.net>
2021-07-20 14:35:57 -0700

    Pure and Impure Pipes (or: How to "fix" the Filter Pipe)

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -1,7 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'filter'
+  name: 'filter',
+  pure: false //default is true
+  // Pipe will recalculate whenever ANYTHING changes on the page
+  // vs purely focusing on whether the pipe arguments changed
+  // Downside: might lead to performance issues for large data, but IF you know what you're doing and need this behavior its fine.
 })
 export class FilterPipe implements PipeTransform {
 
